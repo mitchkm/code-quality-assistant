d77a21e465f09aca690d909adab4f3c7b1b79d81
Jace7Valentine <mitchkm12@gmail.com>
2019-08-06 16:24:08 -0700

    adds json data as a local object for d3

--- web-ui/src/main.ts
+++ web-ui/src/main.ts
@@ -1,18 +1,44 @@
-var canvas = d3.select("body").append("svg")
+import d3 = require("d3");
+
+const data: any = {
+    "name": "Max",
+    "value": 100,
+    "children": [
+        {
+            "name" : "Sylvia",
+            "value":  75,
+            "children": [
+                {"name" : "Craig", "value": 25},
+                {"name" : "Robin", "value": 25},
+                {"name" : "Anna", "value": 25}
+            ]
+        },
+        {
+            "name": "David",
+            "value": 75,
+            "children": [
+                {"name": "Jeff", "value": 25},
+                {"name": "Buffy", "value": 25}
+            ]
+        },
+        {
+            "name" : "Mr X",
+            "value": 75
+        }
+
+    ]
+};
+
+const svg = d3.select("body").append("svg")
     .attr("width", 500)
     .attr("height", 500);
+svg.data(data);
 
-d3.json("myDemo.json")
-.then(function(data){
-    var treemap = d3.tree()
-        .size([500, 500])
-    
-    var nodes = d3.hierarchy(data, function(d){
+const treemap = d3.tree()
+    .size([500, 500]);
+let nodes = d3.hierarchy(data, function(d) {
         return d.children;
-    })
-    nodes = treemap(nodes)
-    console.log(treemap);
-})
-.catch(function(error){
-    console.log(error);
-});
+    });
+
+nodes = treemap(nodes);
+console.log(treemap);
