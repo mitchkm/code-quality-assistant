557e6d183a49556b3d44cb9003b37f28e2993198
Mitchell Miller <mitchkm12@gmail.com>
2019-08-07 16:50:33 -0700

    sets injection point for python script

--- web-ui/src/example.ts
+++ web-ui/src/example.ts
@@ -1,5 +1,10 @@
 // An export for example data to test d3 viz with
-export const exampleData = {
+// This output should be the json outputted by the python code analysis script
+export const exampleData = undefined;
+
+// This output should be the processed version of the python script output
+// Readable by d3
+export const exampleProcessedData = {
   name: "Max",
   value: 100,
   children: [
--- web-ui/src/main.ts
+++ web-ui/src/main.ts
@@ -1,42 +1,23 @@
 import d3 = require("d3");
+import { exampleProcessedData } from "./example";
 
-const data = {
-    "name": "Max",
-    "value": 100,
-    "children": [
-        {
-            "name": "Sylvia",
-            "value": 75,
-            "children": [
-                { "name": "Craig", "value": 25 },
-                { "name": "Robin", "value": 25 },
-                { "name": "Anna", "value": 25 }
-            ]
-        },
-        {
-            "name": "David",
-            "value": 75,
-            "children": [
-                { "name": "Jeff", "value": 25 },
-                { "name": "Buffy", "value": 25 }
-            ]
-        },
-        {
-            "name": "Mr X",
-            "value": 75
-        }
-    ]
-};
 
-var canvas = d3.select("body").append("svg")
+// tslint:disable-next-line: whitespace
+const data: any = {"$replace_me":0};
+
+// TODO process data:
+
+const processedData: any = exampleProcessedData;
+
+const canvas = d3.select("body").append("svg")
     .attr("width", 500)
     .attr("height", 500);
 
-var treemap = d3.treemap()
+const treemap = d3.treemap()
     .size([500, 500])
     .padding(2);
 
-var root = d3.hierarchy(data);
+const root = d3.hierarchy(processedData);
 
 const nodes = treemap(root
     .sum(d => d.value)
@@ -45,25 +26,25 @@ const nodes = treemap(root
 
 console.log(nodes);
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var cells = canvas.selectAll(".cell")
+const cells = canvas.selectAll(".cell")
     .data(nodes)
     .enter()
     .append("g")
     .attr("class", "cell");
 
 cells.append("rect")
-    .attr('x', function (d) { return d.x0 + "px"; })
-    .attr('y', function (d) { return d.y0 + "px"; })
-    .attr('width', function (d) { return (d.x1 - d.x0) + "px"; })
-    .attr('height', function (d) { return d.y1 - d.y0 + "px"; })
+    .attr("x", function (d) { return d.x0 + "px"; })
+    .attr("y", function (d) { return d.y0 + "px"; })
+    .attr("width", function (d) { return (d.x1 - d.x0) + "px"; })
+    .attr("height", function (d) { return d.y1 - d.y0 + "px"; })
     .style("stroke", "white")
-    .style("fill", function (d) { return d.children ? null : color((d.parent.data as any).name); });
+    .style("fill", function (d) { return d.children ? undefined : color((d.parent.data as any).name); });
 
     cells.append("text")
     .attr("x", function (d) { return d.x0 + 5; })
     .attr("y", function (d) { return d.y0 + 20; })
-    .text(function (d) { return d.children ? null : (d.data as any).name; })
+    .text(function (d) { return d.children ? undefined : (d.data as any).name; })
     .attr("font-size", "15px")
     .attr("fill", "white");
