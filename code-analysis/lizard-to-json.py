f02445da6c4a683285d9b5e4b4debde319b59307
Noah Paige <noahlandonpaige@gmail.com>
2019-08-08 15:44:59 -0700

    Removed some useless imports.
    see title. Still unfinished. Working on turning Lizard objects into JSON that is fomatted the way we want it.

--- code-analysis/lizard-to-json.py
+++ code-analysis/lizard-to-json.py
@@ -4,12 +4,10 @@
 
 
 import json
+import lizard
+import glob
 import sys
 import os
-import xmltodict
-import lizard
-import collections
-import pathlib
 
 
 #GLOBALS
@@ -25,76 +23,24 @@ SUPPORTEDLANGUAGES = ["c", "cpp", "cc", "mm", "cxx", "h", "hpp", "cs", "gd",
 def run():
     path = checkPathExists()
     filenameList = buildFilenameList(path)
-    #clean(True, True)
-    lizOut = runLizard3(filenameList)
+    print(filenameList)
+    lizOut = runLizard(filenameList)
     printAllDicts(lizOut)
-    #clean(True, False)
+
 
 # print json for
 def printAllDicts(dictList):
     for d in dictList:
         print(d)
 
-# old function. It is unused, but I am keeping until I have a better solution.
-def printJSON(lizOut):
-    print("PRINTING JSON")
-    print(type(lizOut))
-    if type(lizOut) is collections.OrderedDict:
-        print(lizOut)
-        print("\n\n")
-        print(json.dumps(lizOut))
-        return
-    for item in lizOut:
-        if(type(item) is lizard.FileInformation):
-            print("TYPOOOO")
-            print(type(item))
-        else:
-            printJSON(item)
-
-#just deletes files
-def clean(cleanXMLTempFile, cleanOutputFile):
-    if(cleanOutputFile):
-        if os.path.exists(OUTPUTFILENAME):
-            os.remove(OUTPUTFILENAME)
-
-    if(cleanXMLTempFile):
-        if os.path.exists(XMLTEMPFILENAME):
-            os.remove(XMLTEMPFILENAME)
 
 # Runs lizard on each individual file designated in the given filenameList. Currently the only working on this one.
-def runLizard3(filenameList):
+def runLizard(filenameList):
     outList = []
     for file in filenameList:
-        outList.append((lizard.analyze_file(file)).__dict__)
+        outList.append((lizard.analyze_file(file)))
     return outList
 
-# old function and doesn't work fully. Keeping this old code until I have a definite solution.
-# runs lizard on a given path by using lizard as a python extension.
-def runLizard2(path):
-    i = lizard.analyze(path)
-    print(type(i))
-    if(type(i) is lizard.FileInformation):
-        print("FOUND FILE INFORMATION")
-        returnme = list()
-        returnme.append(i)
-        return returnme
-    print("FOUND ITERATOR")
-    return i
-
-# old function and doesn't work fully. Keeping this old code until I have a definite solution.
-# runs lizard on a given path through an os command. Outputs to temp xml file, then converts xml to json.
-def runLizard(path):
-    command = "lizard --xml " + path + " >> " + XMLTEMPFILENAME
-    os.system(command)
-    jsonDict = convertXMLtoJSON()
-    return jsonDict
-
-
-def convertXMLtoJSON():
-    xmlData = open(XMLTEMPFILENAME, 'r').read()
-    jsonData = xmltodict.parse(xmlData)
-    xmlData.close()
-    return jsonData
 
 # returns the input string if no errors, signals error and quits else
 def checkPathExists():
@@ -102,6 +48,8 @@ def checkPathExists():
         print("lizard-to-json error: No path given.")
         sys.exit()
     path = sys.argv[1]
+    if (path[-1] is not '/') and os.path.isdir(path):
+        path += "/"
     if(os.path.exists(path) is not True):
         print("lizard-to-json error: Path \'", path, "\' does not exist.")
         sys.exit()
@@ -110,28 +58,32 @@ def checkPathExists():
         print(path)
     return path
 
-
 def buildFilenameList(path):
-    print("BUILDING NAME LIST")
-    print(path)
-    nameList = []
-    if(os.path.isdir(path)):
-        entriesInDir = os.listdir(path)
-        for entry in entriesInDir:
-            fullPath = path
-            if fullPath[-1] is not "/": fullPath += "/"
-            fullPath += entry + "/"
-            nameList.extend(buildFilenameList(fullPath))
-    else:
+    files = []
+    if(os.path.isfile(path)):
         if getExtensionFromFilename(path) in SUPPORTEDLANGUAGES:
-            nameList.append(path)
-    return nameList
+            files.append(path)
+            return files
+    for ext in SUPPORTEDLANGUAGES:
+        addUs = [f for f in glob.glob(path + "**/*." + ext, recursive=True)]
+        files.extend(addUs)
+    return files
 
 
 def getExtensionFromFilename(path):
     ext = pathlib.Path(path).suffix
     ext = ext[1:] # strip off the '.'
     return ext
+
+#def fileInfoToDict(fileInfo):
+
+
+def funcInfoToDict(funcInfo):
+    return { 'name' : funcInfo.name,
+             'nloc' : funcInfo.nloc,
+             'ccn'  : funcInfo.cyclomatic_complexity,
+             'tok'  : funcInfo.token_count,
+             'len'  : funcInfo.length}
 
 
 run()
