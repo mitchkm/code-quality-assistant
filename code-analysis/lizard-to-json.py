091c0b1c7d6e6b46ff8d013e537bc4d791221ecb
Mitchell Miller <mitchkm12@gmail.com>
2019-08-14 15:30:20 -0700

    changes python script to create a folder with web-ui in it

--- code-analysis/lizard-to-json.py
+++ code-analysis/lizard-to-json.py
@@ -1,3 +1,4 @@
+# Make this the actual full script
 # runs lizard on given file/directory and then outputs the .json formatted results to the command line
 # input : file or directory to run lizard analysis on
 # output: JSON formatted results of lizard analysis to the command line
@@ -9,29 +10,42 @@ import glob
 import sys
 import os
 import pathlib
+from distutils.dir_util import copy_tree
 
 
 #GLOBALS
-SUPPORTEDLANGUAGES = ["c", "cpp", "cc", "mm", "cxx", "h", "hpp", "cs", "gd",
+SUPPORTED_LANGUAGES = ["c", "cpp", "cc", "mm", "cxx", "h", "hpp", "cs", "gd",
                       "go", "java", "js", "lua", "m", "php", "py", "rb",
                       "scala", "swift", "tnsdl", "sdl", "ttcn", "ttcnpp"]
+OUT_DIR = "./code-quality-assistant"
 
+# For packaged data files
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 # runs it all. This is called down below.
 def run():
     path = checkPathExists()
     filenameList = buildFilenameList(path)
-    print(filenameList)
+    # print(filenameList)
     lizOut = runLizard(filenameList)
     asDict = fileListToDict(path, lizOut)
-    print(json.dumps(asDict, indent=4, sort_keys=False))
-
+    rawData = json.dumps(asDict, indent=4, sort_keys=False)
+    createWebFiles()
+    injectData(rawData)
 
 # Runs lizard on each individual file designated in the given filenameList.
 def runLizard(filenameList):
     outList = []
     for file in filenameList:
-        outList.append((lizard.analyze_file(file)))
+        outList.append((lizard.analyze_file(file))) 
     return outList
 
 
@@ -51,14 +65,29 @@ def checkPathExists():
         print(path)
     return path
 
-#goes through given path and identifies all files within that are supported by lizard, then puts those files in a list
+# creates necessary web-ui files
+def createWebFiles():
+    copy_tree(resource_path("web-ui"), OUT_DIR)
+
+# injects rawData aquired into html file to be accessible for web visualization
+def injectData(json):
+    file_in = OUT_DIR + "/index.html"
+    file_out = OUT_DIR + "/tmp.txt"
+    with open(file_in, "rt") as fin:
+        with open(file_out, "wt") as fout:
+            for line in fin:
+                fout.write(line.replace("{code_analysis_json}", json))
+
+    os.rename(file_out, file_in)
+
+# goes through given path and identifies all files within that are supported by lizard, then puts those files in a list
 def buildFilenameList(path):
     files = []
     if(os.path.isfile(path)):
-        if getExtensionFromFilename(path) in SUPPORTEDLANGUAGES:
+        if getExtensionFromFilename(path) in SUPPORTED_LANGUAGES:
             files.append(path)
             return files
-    for ext in SUPPORTEDLANGUAGES:
+    for ext in SUPPORTED_LANGUAGES:
         addUs = [f for f in glob.glob(path + "**/*." + ext, recursive=True)]
         files.extend(addUs)
     return files
