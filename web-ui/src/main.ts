bde061d95f7b9fee8054acea113e134e978f21dd
aju960219 <aju960219@gmail.com>
2019-08-19 15:36:33 -0700

    fix to be able to change size and color options

--- web-ui/src/main.ts
+++ web-ui/src/main.ts
@@ -1,4 +1,20 @@
-import Display from "./display";
+import d3 = require("d3");
+import Treemap from "./treemap";
+
+ // declare width and height of treemap
+const width = 100;
+const height = 100;
+const gamma = 1.0; // change the brightness of color
+const sizeOption = "nloc";
+const colorOption = "ccn";
+
+// set up the color for treemap
+const color = d3.scaleLinear<string>()
+                .domain([0, 0.3333, 0.66666, 1])
+                .range(["green", "yellow", "orange", "red"])
+                .interpolate(d3.interpolateRgb.gamma(gamma));
 
 // display treemap
-Display.display("nloc", "ccn");
+Treemap.display(width, height, sizeOption, colorOption, color);
+// Treemap.display(width, height, colorOption, sizeOption, color);
+
