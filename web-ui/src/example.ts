26612f7bf60102fff388afbc099aa82b26c9c32f
aju960219 <aju960219@gmail.com>
2019-08-23 11:12:16 -0700

    add more files to example data

--- web-ui/src/example.ts
+++ web-ui/src/example.ts
@@ -72,6 +72,216 @@ export const exampleData = {
           length: 8
         }
       }
+    },
+    "1": {
+        filename:
+          "/mnt/g/Projects/apple-surp-2019/code-analysis/examples-to-analyze/analyze-you.swift",
+        functions: {
+          "0": {
+            name: "viewDidLoad",
+            nloc: 10,
+            ccn: 5,
+            tokens: 117,
+            params: 0,
+            length: 25
+          },
+          "1": {
+            name: "deinit",
+            nloc: 3,
+            ccn: 15,
+            tokens: 11,
+            params: 0,
+            length: 3
+          },
+          "2": {
+            name: "adjustInsetForKeyboard",
+            nloc: 20,
+            ccn: 1,
+            tokens: 69,
+            params: 2,
+            length: 9
+          },
+          "3": {
+            name: "dismissKeyboard",
+            nloc: 5,
+            ccn: 2,
+            tokens: 11,
+            params: 0,
+            length: 3
+          },
+          "4": {
+            name: "keyboardWillShow",
+            nloc: 9,
+            ccn: 5,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "5": {
+            name: "keyboardWillHide",
+            nloc: 10,
+            ccn: 2,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "6": {
+            name: "openZoomingController",
+            nloc: 30,
+            ccn: 10,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "7": {
+            name: "prepare",
+            nloc: 10,
+            ccn: 4,
+            tokens: 42,
+            params: 2,
+            length: 8
+          }
+        }
+    },
+    "2": {
+        filename:
+          "/mnt/g/Projects/apple-surp-2019/code-analysis/examples-to-analyze/analyze-them.swift",
+        functions: {
+          "0": {
+            name: "viewDidLoad",
+            nloc: 22,
+            ccn: 2,
+            tokens: 117,
+            params: 0,
+            length: 25
+          },
+          "1": {
+            name: "deinit",
+            nloc: 3,
+            ccn: 1,
+            tokens: 11,
+            params: 0,
+            length: 3
+          },
+          "2": {
+            name: "adjustInsetForKeyboard",
+            nloc: 9,
+            ccn: 2,
+            tokens: 69,
+            params: 2,
+            length: 9
+          },
+          "3": {
+            name: "dismissKeyboard",
+            nloc: 3,
+            ccn: 1,
+            tokens: 11,
+            params: 0,
+            length: 3
+          },
+          "4": {
+            name: "keyboardWillShow",
+            nloc: 3,
+            ccn: 1,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "5": {
+            name: "keyboardWillHide",
+            nloc: 3,
+            ccn: 1,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "6": {
+            name: "openZoomingController",
+            nloc: 3,
+            ccn: 1,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "7": {
+            name: "prepare",
+            nloc: 8,
+            ccn: 4,
+            tokens: 42,
+            params: 2,
+            length: 8
+          }
+        }
+    },
+    "3": {
+        filename:
+          "/mnt/g/Projects/apple-surp-2019/code-analysis/examples-to-analyze/analyze-us.swift",
+        functions: {
+          "0": {
+            name: "viewDidLoad",
+            nloc: 26,
+            ccn: 10,
+            tokens: 117,
+            params: 0,
+            length: 25
+          },
+          "1": {
+            name: "deinit",
+            nloc: 38,
+            ccn: 2,
+            tokens: 11,
+            params: 0,
+            length: 3
+          },
+          "2": {
+            name: "adjustInsetForKeyboard",
+            nloc: 59,
+            ccn: 5,
+            tokens: 69,
+            params: 2,
+            length: 9
+          },
+          "3": {
+            name: "dismissKeyboard",
+            nloc: 100,
+            ccn: 17,
+            tokens: 11,
+            params: 0,
+            length: 3
+          },
+          "4": {
+            name: "keyboardWillShow",
+            nloc: 40,
+            ccn: 6,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "5": {
+            name: "keyboardWillHide",
+            nloc: 20,
+            ccn: 2,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "6": {
+            name: "openZoomingController",
+            nloc: 15,
+            ccn: 1,
+            tokens: 18,
+            params: 1,
+            length: 3
+          },
+          "7": {
+            name: "prepare",
+            nloc: 120,
+            ccn: 30,
+            tokens: 60,
+            params: 2,
+            length: 8
+          }
+        }
     }
   }
 };
@@ -83,6 +293,38 @@ export const exampleProcessedData = {
   value: 54,
   value2: 13,
   children: [
+    {
+      name:
+        "/mnt/g/Projects/apple-surp-2019/code-analysis/examples-to-analyze/analyze-me.swift",
+      value: 54,
+      value2: 13,
+      children: [
+        { name: "viewDidLoad", value: 22, value2: 2, children: undefined },
+        { name: "deinit", value: 3, value2: 1, children: undefined },
+        { name: "adjustInsetForKeyboard", value: 9, value2: 2, children: undefined },
+        { name: "dismissKeyboard", value: 3, value2: 1, children: undefined },
+        { name: "keyboardWillShow", value: 3, value2: 1, children: undefined },
+        { name: "keyboardWillHide", value: 3, value2: 1, children: undefined },
+        { name: "openZoomingController", value: 3, value2: 1, children: undefined },
+        { name: "prepare", value: 8, value2: 4, children: undefined }
+      ]
+    },
+    {
+      name:
+        "/mnt/g/Projects/apple-surp-2019/code-analysis/examples-to-analyze/analyze-you.swift",
+      value: 54,
+      value2: 13,
+      children: [
+        { name: "viewDidLoad", value: 22, value2: 1, children: undefined },
+        { name: "deinit", value: 3, value2: 1, children: undefined },
+        { name: "adjustInsetForKeyboard", value: 9, value2: 2, children: undefined },
+        { name: "dismissKeyboard", value: 3, value2: 1, children: undefined },
+        { name: "keyboardWillShow", value: 3, value2: 1, children: undefined },
+        { name: "keyboardWillHide", value: 3, value2: 1, children: undefined },
+        { name: "openZoomingController", value: 3, value2: 1, children: undefined },
+        { name: "prepare", value: 8, value2: 4, children: undefined }
+      ]
+    },
     {
       name:
         "/mnt/g/Projects/apple-surp-2019/code-analysis/examples-to-analyze/analyze-me.swift",
