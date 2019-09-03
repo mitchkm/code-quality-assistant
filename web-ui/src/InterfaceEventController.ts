8ddd30981ffe0889868db82c513af25d21102de3
Noah Paige <noahlandonpaige@gmail.com>
2019-09-03 16:58:30 -0700

    Added more descriptive id's in the html

--- web-ui/src/InterfaceEventController.ts
+++ web-ui/src/InterfaceEventController.ts
@@ -1,6 +1,6 @@
 class InterfaceEventController {
   constructor() {
-    const chartSelectButtons: HTMLElement [] = [document.getElementById("treemapChart"), document.getElementById("chart2"), document.getElementById("chart3")];
+    const chartSelectButtons: HTMLElement [] = [document.getElementById("treemapChartButton"), document.getElementById("chart2Button"), document.getElementById("chart3Button")];
     let currentChart = chartSelectButtons[0];
     currentChart.className += " active";
     // Loop through the buttons and add the active class to the current/clicked button
