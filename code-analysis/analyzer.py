ba37ba1abd64d076503123e51171fb4355f2136e
Mitchell Miller <mitchkm12@gmail.com>
2019-09-04 01:11:15 -0700

    seperates python script into wrapper (main.py) and analyzer code

---
+++ code-analysis/analyzer.py
@@ -0,0 +1,51 @@
+# Code Analysis module
+# provides functions to analyze a list of files
+# the analyze funciton returns a json string to be injected into a web-ui
+
+import json
+import lizard
+
+#GLOBALS
+LIZARD_SUPPORTED_LANGUAGES = ["c", "cpp", "cc", "mm", "cxx", "h", "hpp", "cs", "gd",
+                      "go", "java", "js", "lua", "m", "php", "py", "rb",
+                      "scala", "swift", "tnsdl", "sdl", "ttcn", "ttcnpp"]
+
+# analyzes all files, with lizard, provided in list and returns a json string of the raw data
+def lizAnalyze(path, filenameList):
+    lizOut = runLizard(filenameList)
+    asDict = fileListToDict(path, lizOut)
+    data = json.dumps(asDict, indent=4, sort_keys=False)
+    return data
+
+# Runs lizard on each individual file designated in the given filenameList.
+def runLizard(filenameList):
+    outList = []
+    for file in filenameList:
+        outList.append((lizard.analyze_file(file))) 
+    return outList
+
+# converts a list of FileInformation objects into a list of dictionaries
+def fileListToDict(path, fileList):
+    filesAsDicts = [fileInfoToDict(f) for f in fileList]
+
+    return { 'path'  : path,
+             'files' : {str(i) : filesAsDicts[i] for i in range(0, len(filesAsDicts))}}
+
+# converts FileInformation object into a dictionary
+def fileInfoToDict(fileInfo):
+    filename = fileInfo.filename
+    funcs = []
+    for func in fileInfo.function_list:
+        funcs.append(funcInfoToDict(func))
+    funcsDict = {str(i) : funcs[i] for i in range(0, len(funcs))}
+    return {'filename'  : filename,
+            'functions' : funcsDict}
+
+# converts FunctionInformation object into dictionary
+def funcInfoToDict(funcInfo):
+    return { 'name'   : funcInfo.name,
+             'nloc'   : funcInfo.nloc,
+             'ccn'    : funcInfo.cyclomatic_complexity,
+             'tokens' : funcInfo.token_count,
+             'params' : len(funcInfo.parameters),
+             'length' : funcInfo.length}
--- code-analysis/main.py
+++ code-analysis/main.py
@@ -1,52 +1,26 @@
-# Make this the actual full script
-# runs lizard on given file/directory and then outputs the .json formatted results to the command line
-# input : file or directory to run lizard analysis on
-# output: JSON formatted results of lizard analysis to the command line
+# Code Quality Assistant
+# input : file or directory to run code analysis on
+# output: hmtl website to analyze code analysis results found ./code-quality-assistant directory
 
+import analyzer
 
-import json
-import lizard
 import glob
 import sys
 import os
 import pathlib
 from distutils.dir_util import copy_tree
 
-
-#GLOBALS
-SUPPORTED_LANGUAGES = ["c", "cpp", "cc", "mm", "cxx", "h", "hpp", "cs", "gd",
-                      "go", "java", "js", "lua", "m", "php", "py", "rb",
-                      "scala", "swift", "tnsdl", "sdl", "ttcn", "ttcnpp"]
+# GLOBALS
 OUT_DIR = "./code-quality-assistant"
-
-# For packaged data files
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
+DATA_REPLACE_TOKEN = "{code_analysis_json}"
 
 # runs it all. This is called down below.
 def run():
     path = checkPathExists()
     filenameList = buildFilenameList(path)
-    # print(filenameList)
-    lizOut = runLizard(filenameList)
-    asDict = fileListToDict(path, lizOut)
-    rawData = json.dumps(asDict, indent=4, sort_keys=False)
+    jsonData = analyzer.lizAnalyze(path, filenameList)
     createWebFiles()
-    injectData(rawData)
-
-# Runs lizard on each individual file designated in the given filenameList.
-def runLizard(filenameList):
-    outList = []
-    for file in filenameList:
-        outList.append((lizard.analyze_file(file))) 
-    return outList
+    injectData(jsonData)
 
 
 # returns the input string if no errors, signals error and quits else
@@ -62,21 +36,31 @@ def checkPathExists():
         sys.exit()
     if path[0] is not '/':
         path = os.getcwd() + "/" + path
-        # print(path)
     return path
 
-# creates necessary web-ui files
+# for packaged data files in the stand alone executable
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+# creates necessary web-ui files 
 def createWebFiles():
     copy_tree(resource_path("web-ui"), OUT_DIR)
 
-# injects rawData aquired into html file to be accessible for web visualization
+# injects json data aquired into html file to be accessible for web visualization
 def injectData(json):
     file_in = OUT_DIR + "/index.html"
     file_out = OUT_DIR + "/tmp.txt"
     with open(file_in, "rt") as fin:
         with open(file_out, "wt") as fout:
             for line in fin:
-                fout.write(line.replace("{code_analysis_json}", json))
+                fout.write(line.replace(DATA_REPLACE_TOKEN, json))
 
     os.rename(file_out, file_in)
 
@@ -84,10 +68,10 @@ def injectData(json):
 def buildFilenameList(path):
     files = []
     if(os.path.isfile(path)):
-        if getExtensionFromFilename(path) in SUPPORTED_LANGUAGES:
+        if getExtensionFromFilename(path) in analyzer.LIZARD_SUPPORTED_LANGUAGES:
             files.append(path)
             return files
-    for ext in SUPPORTED_LANGUAGES:
+    for ext in analyzer.LIZARD_SUPPORTED_LANGUAGES:
         addUs = [f for f in glob.glob(path + "**/*." + ext, recursive=True)]
         files.extend(addUs)
     return files
@@ -97,32 +81,5 @@ def getExtensionFromFilename(path):
     ext = pathlib.Path(path).suffix
     ext = ext[1:] # strip off the '.'
     return ext
-
-# converts a list of FileInformation objects into a list of dictionaries
-def fileListToDict(path, fileList):
-    filesAsDicts = [fileInfoToDict(f) for f in fileList]
-
-    return { 'path'  : path,
-             'files' : {str(i) : filesAsDicts[i] for i in range(0, len(filesAsDicts))}}
-
-# converts FileInformation object into a dictionary
-def fileInfoToDict(fileInfo):
-    filename = fileInfo.filename
-    funcs = []
-    for func in fileInfo.function_list:
-        funcs.append(funcInfoToDict(func))
-    funcsDict = {str(i) : funcs[i] for i in range(0, len(funcs))}
-    return {'filename'  : filename,
-            'functions' : funcsDict}
-
-# converts FunctionInformation object into dictionary
-def funcInfoToDict(funcInfo):
-    return { 'name'   : funcInfo.name,
-             'nloc'   : funcInfo.nloc,
-             'ccn'    : funcInfo.cyclomatic_complexity,
-             'tokens' : funcInfo.token_count,
-             'params' : len(funcInfo.parameters),
-             'length' : funcInfo.length}
-
 
 run()
