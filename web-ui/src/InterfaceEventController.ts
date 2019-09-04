d6cb1da7c2cab197945b0455a419738e56e0f24a
Mitchell Miller <mitchkm12@gmail.com>
2019-09-04 11:55:04 -0700

    changes interface event controller to namespace with static functions

--- web-ui/src/InterfaceEventController.ts
+++ web-ui/src/InterfaceEventController.ts
@@ -1,12 +1,15 @@
 class InterfaceEventController {
-  constructor() {
-    const chartSelectButtons: HTMLElement [] = [document.getElementById("treemapChartButton"), document.getElementById("chart2Button"), document.getElementById("chart3Button")];
+  static init() {
+    const chartSelectButtons: HTMLElement[] = [
+      document.getElementById("treemapChartButton"),
+      document.getElementById("chart2Button"),
+      document.getElementById("chart3Button")
+    ];
     let currentChart = chartSelectButtons[0];
     currentChart.className += " active";
     // Loop through the buttons and add the active class to the current/clicked button
     for (let i = 0; i < chartSelectButtons.length; i++) {
-      chartSelectButtons[i].addEventListener("click", function () {
-        console.log("handling chart change");
+      chartSelectButtons[i].addEventListener("click", function() {
         currentChart.className = "";
         this.className = "active";
         currentChart = this;
