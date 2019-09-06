985d912b76895f04d50844aab20c8fa1585c22e5
aju960219 <aju960219@gmail.com>
2019-09-06 13:15:04 -0700

    implements showing metrics for each function when mouse hovered

--- web-ui/src/Treemap/treemap.ts
+++ web-ui/src/Treemap/treemap.ts
@@ -1,7 +1,6 @@
 import d3 = require("d3");
 import TreemapSetting from "./treemapSetting";
 import TreemapData from "../Data/treemapData";
-import { FunctionData } from "../Data/metricData";
 
 class Treemap {
   processedData: TreemapData;
@@ -27,20 +26,26 @@ class Treemap {
     const paddingLeft = this.treemapSetting.paddings[2];
     const paddingRight = this.treemapSetting.paddings[3];
 
+    // determines the width of each square in a treemap
     this.xScale = d3
       .scaleLinear()
       .domain([0, this.treemapSetting.width])
       .range([0, this.treemapSetting.width]);
+
+    // determines the height of each square in a treemap
     this.yScale = d3
       .scaleLinear()
       .domain([0, this.treemapSetting.height])
       .range([0, this.treemapSetting.height]);
+
+    // decides the color of each sqaure in a treemap
     this.color = d3
       .scaleLinear<string>()
       .domain(this.treemapSetting.color.thresholds)
       .range(this.treemapSetting.color.colors)
       .interpolate(d3.interpolateRgb.gamma(this.treemapSetting.color.gamma));
 
+    // sets treemap layout
     const treemap = d3
       .treemap()
       .size([this.treemapSetting.width, this.treemapSetting.height])
@@ -57,8 +62,13 @@ class Treemap {
    * Create nodes of a treemap based on the hierarchy of data
    */
   private createTreeNodes() {
+
+    // creates hierarchy of data
     const root = d3.hierarchy(this.processedData);
+
     const treemap = this.createTreemap();
+
+    // sorts and applies nodes to treemap layout
     const nodes = treemap(
       root.sort(d => d.value).sort((a, b) => a.value - b.value)
     );
@@ -70,11 +80,13 @@ class Treemap {
    * @param nodes treemap nodes to create treemap with
    */
   private setUpTreemapChart(nodes) {
+
     // deleting previous cell style
     if (d3.select(".node")) {
       d3.selectAll(".node").remove();
     }
 
+    // dynamically creates HTML <div> elements for nodes
     const chart = d3
       .select("#treeMapChart")
       .selectAll(".node")
@@ -95,19 +107,23 @@ class Treemap {
    * @param processedData data to update the treemap
    */
   drawTreemap(processedData?: TreemapData) {
+
+    // change to new data from user
     if (processedData) {
       this.processedData = processedData;
     }
-    const nodes = this.createTreeNodes();
-    const chart = this.setUpTreemapChart(nodes);
-    const upButton = d3.select("#TreemapBackButton").datum(nodes);
-    // console.log(this.processedData);
-    // console.log(nodes);
 
-    const fileNames = d3
+    const nodes = this.createTreeNodes();
+
+    const chart = this.setUpTreemapChart(nodes);
+
+    const upButton = d3.select("#TreemapBackButton").datum(nodes);
+
+    const mouseHover = d3
       .select("#treeMapCard")
       .append("div")
-      .attr("class", "fileNames");
+      .attr("class", "mouseHover");
+
     chart
       .style("left", (d: any) => {
         return this.xScale(d.x0) + "%";
@@ -131,35 +147,24 @@ class Treemap {
           : "none";
       })
       .on("click", (d: any) => {
+        // on click zoom-in treemap
         this.zoomTreemap(d, chart, upButton, nodes);
       })
       .append("p")
       .attr("class", "label")
       .text((d: any) => {
-        if (d.depth === 2) {
-          if (d.data.name) {
-            let funcData = "";
-            funcData += "\nfunctionName: " + d.data.funcData.name;
-            funcData += "\nfunctionLongName: " + d.data.funcData.longName;
-            funcData += "\nstartLine: " + d.data.funcData.startLine;
-            funcData += "\nnloc: " + d.data.funcData.nloc;
-            funcData += "\nccn: " + d.data.funcData.ccn;
-            funcData += "\ntokens: " + d.data.funcData.tokens;
-            funcData += "\nparams: " + d.data.funcData.params;
-            funcData += "\nlength: " + d.data.funcData.length;
-            return funcData;
-          }
-        }
         return d.data.name ? d.data.name : "---";
       })
-      .on("mouseover", (d: any) => {
-        fileNames.style("left", d3.event.pageX + 10 + "px");
-        fileNames.style("top", d3.event.pageY - 20 + "px");
-        fileNames.style("display", "inline-block");
-        fileNames.html(d.data.name);
+      .on("mousemove", (d: any) => {
+        // display file/function name when mouse hovered
+        mouseHover.style("left", d3.event.pageX + 10 + "px");
+        mouseHover.style("top", d3.event.pageY - 20 + "px");
+        mouseHover.style("display", "inline-block");
+        mouseHover.text(d.data.name);
       })
-      .on("mouseout", d => {
-        fileNames.style("display", "none");
+      .on("mouseout", () => {
+        // stop displaying file/function name when mouse out
+        mouseHover.style("display", "none");
       });
 
     // hide the text when it's fully zoomed-out
@@ -167,6 +172,7 @@ class Treemap {
       return d.depth > 1 ? 0 : 1;
     });
 
+    // on click zoom out treemap
     upButton.on("click", d => {
       this.zoomTreemap(d, chart, upButton, nodes);
     });
@@ -175,21 +181,57 @@ class Treemap {
 
   /**
    * Handles zooming in the treemap
-   * @param d current node in the treemap
+   * @param d clicked node in the treemap
    * @param chart treemapChart created by setUpTreemapChart function
    * @param upButton navigates treemap to zoom out
    * @param nodes entire nodes in the treemap
    */
   private zoomTreemap(d, chart, upButton, nodes) {
+
     const currentDepth = d.depth;
+
+    // changes domains of width and height according to selected node
     this.xScale.domain([d.x0, d.x1]);
     this.yScale.domain([d.y0, d.y1]);
+
     upButton.datum(d.parent || nodes);
 
+    // sets transition
     const t = d3
       .transition()
       .duration(800)
       .ease(d3.easeCubicOut);
+
+    // changes message when treemap is hovered and fully zoomed in
+    const mouseHover = d3.select(".mouseHover");
+
+    if (currentDepth === 2) {
+      chart
+        .on("mousemove", (d) => {
+          let funcData = "";
+          funcData += "functionName: " + d.data.funcData.name;
+          funcData += "\nfunctionFullName: " + d.data.funcData.longName;
+          funcData += "\nstartLine: " + d.data.funcData.startLine;
+          funcData += "\nnloc: " + d.data.funcData.nloc;
+          funcData += "\nccn: " + d.data.funcData.ccn;
+          funcData += "\ntokens: " + d.data.funcData.tokens;
+          funcData += "\nparams: " + d.data.funcData.params;
+          funcData += "\nlength: " + d.data.funcData.length;
+          mouseHover.text(funcData);
+        })
+        .on("mouseout", () => {
+          mouseHover.style("display", "none");
+        });
+    } else {
+      // display file/function name when treemap is not fully zoomed in and gets mouse hovered
+      chart
+        .on("mousemove", (d) => {
+          mouseHover.text(d.data.name);
+        })
+        .on("mouseout", () => {
+          mouseHover.style("display", "none");
+        });
+    }
 
     chart
       .transition(t)
