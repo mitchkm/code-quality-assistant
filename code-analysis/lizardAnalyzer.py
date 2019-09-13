2fbc0423422bf98e884036a9cd96af3dd733bec8
Mitchell Miller <mitchkm12@gmail.com>
2019-09-13 13:53:17 -0700

    renames analyzer.py to lizardAnalyzer.py for clarity (#19) adds functionality for duplicate detection and some json restructure noted in metricData.ts' types

--- code-analysis/lizardAnalyzer.py
+++ code-analysis/lizardAnalyzer.py
@@ -1,4 +1,4 @@
-# Code Analysis module
+# Code Analysis module via Lizard code analyzer
 # provides functions to analyze a list of files
 # the analyze funciton returns a json string to be injected into a web-ui
 
@@ -10,12 +10,18 @@ from lizard_ext.lizardduplicate import LizardExtension as DuplicateDetector
 LIZARD_SUPPORTED_LANGUAGES = ["c", "cpp", "cc", "mm", "cxx", "h", "hpp", "cs", "gd",
                       "go", "java", "js", "lua", "m", "php", "py", "rb",
                       "scala", "swift", "tnsdl", "sdl", "ttcn", "ttcnpp"]
+# How many tokens such as: "if", "(", "abc", "%", "4", ")" must be repeated to appear in duplicate detection list
+MIN_DUP_TOKENS = 70 
 
 # analyzes all files, with lizard, provided in list and returns a json string of the raw data
-def lizAnalyze(path, filenameList):
+def analyze(path, filenameList):
     lizOut = runLizard(filenameList)
-    asDict = fileListToDict(path, lizOut)
+    asDict = filesInfoToDict(path, lizOut)
+
+    # for more readable json output:
     data = json.dumps(asDict, indent=4, sort_keys=False)
+    # for compressed json:
+    # data = json.dumps(asDict, sort_keys=False)
     return data
 
 # Runs lizard on each individual file designated in the given filenameList.
@@ -23,14 +29,19 @@ def runLizard(filenameList):
     duplicates = DuplicateDetector()
     extensions = lizard.get_extensions([duplicates])
     outList = list(lizard.analyze_files(filenameList, exts=extensions))
-    duplicates.print_result()
-    return outList
+    dupCodeSnips = list(duplicates.get_duplicates(min_duplicate_tokens=MIN_DUP_TOKENS))
+    dupInfo = { 'duplicates': [dupInfoToDict(d) for d in dupCodeSnips], 
+                'duplicateRate': duplicates.saved_duplicate_rate, 
+                'uniqueRate': duplicates.saved_unique_rate }
+    return { 'fileList': outList, 
+             'dupInfo': dupInfo }
 
 # converts a list of FileInformation objects into a list of dictionaries
-def fileListToDict(path, fileList):
-    filesAsDicts = [fileInfoToDict(f) for f in fileList]
+def filesInfoToDict(path, info):
+    filesAsDicts = [fileInfoToDict(f) for f in info['fileList']]
 
     return { 'path'  : path,
+             'duplicateInfo': info['dupInfo'],
              'files' : filesAsDicts }
 
 # converts FileInformation object into a dictionary
@@ -41,6 +52,7 @@ def fileInfoToDict(fileInfo):
         funcs.append(funcInfoToDict(func))
     
     return {'filename'  : filename,
+            'filetype'  : filename.split('.')[1],
             'functions' : funcs}
 
 # converts FunctionInformation object into dictionary
@@ -53,3 +65,9 @@ def funcInfoToDict(funcInfo):
              'tokens' : funcInfo.token_count,
              'params' : len(funcInfo.parameters),
              'length' : funcInfo.length}
+
+# converts duplicate CodeSnippet objects to dicts of info
+def dupInfoToDict(snippets):    
+    return [{ 'filename': c.file_name,
+              'startLine': c.start_line, 
+              'endLine': c.end_line} for c in snippets]
