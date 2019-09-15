727d6a14792af78c46f1908979066bc3673eea02
Noah Paige <noahlandonpaige@gmail.com>
2019-09-15 16:11:23 -0700

    Added Duplication rate svg donut graph

--- web-ui/src/EventController/InterfaceEventController.ts
+++ web-ui/src/EventController/InterfaceEventController.ts
@@ -69,6 +69,19 @@ class InterfaceEventController {
     });
     util.fillURLText(undefined);
   }
+
+  public static initDonut(dupRate: number){
+    this.updateDonut(dupRate);
+  }
+
+  public static updateDonut(dupRate: number){
+    const donutElement = d3.select("#donutElement");
+    const percentElement = document.getElementById("donutPercent");
+    const attrString = this.dupRateToAttributeString(dupRate);
+    donutElement.attr("stroke-dasharray", attrString);
+    percentElement.textContent = (Math.round(dupRate * 10) / 10).toString() + "%";
+  }
+
   public static initDuplicates(mD: MetricData) {
     const dupList = d3.select("#duplicatesList").select(".card-columns");
     dupList.selectAll("div").remove();
@@ -122,6 +135,11 @@ class InterfaceEventController {
           .text(dup.endLine);
       });
     });
+  }
+
+  private static dupRateToAttributeString(dupRate: number) {
+    const percentage = Math.round(dupRate * 10) / 10;
+    return percentage.toString() + " " + (100 - percentage).toString();
   }
 }
 
